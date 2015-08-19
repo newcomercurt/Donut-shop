27f9c8d7bcc3a6b9ecde3c4f2839e64821d62eb8
newcomercurt <newcomercurt@gmail.com>
2015-08-18 21:39:57 -0700

    August 18th changes
    Fixed size adjustment on comment box, stylized comment box to match rest
    of page, made page responsive to smaller screens, amended comment box to
    only submit with text input, allowed user to submit comment on enter
    key, made comments newest comments show up first

--- javascript.js
+++ javascript.js
@@ -1,73 +1,83 @@
-var Shop = function(shopHours,custMin, custMax, custAvg, shopName){
-	this.hours = shopHours;
-	this.min = custMin;
-	this.max = custMax;
-	this.avg = custAvg;
-	this.name = shopName;
-	this.customersPerHour = [];
-	this.donutsPerHour = [];
-	this.donutsPerDay = 0;
-	for (var i = 0; i < this.hours; i++) {
-		var customersThisHour = getRandomInt(custMin, custMax);
-		var donutsSoldThishour = custAvg * customersThisHour;
-		this.customersPerHour[i] = customersThisHour;
-		this.donutsPerHour[i] = donutsSoldThishour;
-		this.donutsPerDay += donutsSoldThishour;
-	}
-	console.log(this);
+var Shop = function(shopHours, custMin, custMax, custAvg, shopName) {
+    this.hours = shopHours;
+    this.min = custMin;
+    this.max = custMax;
+    this.avg = custAvg;
+    this.name = shopName;
+    this.customersPerHour = [];
+    this.donutsPerHour = [];
+    this.donutsPerDay = 0;
+    for (var i = 0; i < this.hours; i++) {
+        var customersThisHour = getRandomInt(custMin, custMax);
+        var donutsSoldThishour = custAvg * customersThisHour;
+        this.customersPerHour[i] = customersThisHour;
+        this.donutsPerHour[i] = donutsSoldThishour;
+        this.donutsPerDay += donutsSoldThishour;
+    }
+    console.log(this);
 }
-function average(array){
-	var length=array.length;
-	return array.reduce(function(total, next){
-		return total+next
-	}, 0)/length;
+
+function average(array) {
+    var length = array.length;
+    return array.reduce(function(total, next) {
+        return total + next
+    }, 0) / length;
 
 }
 
 function getRandomInt(custMin, custMax) {
-		return Math.floor(Math.random() * (custMax - custMin)) + custMin;
+    return Math.floor(Math.random() * (custMax - custMin)) + custMin;
 }
-var shops =[
-	new Shop(8, 8, 43, 4.5, "Downtown"),
-	new Shop(24, 4, 37, 2, "Capitol Hill"),
-	new Shop(10, 9, 23, 6.33, "South Lake Union"),
-	new Shop(7, 2, 28, 1.25, "Wedgewood"),
-	new Shop(10, 8, 58, 3.75, "Ballard")
+var shops = [
+    new Shop(8, 8, 43, 4.5, "Downtown"),
+    new Shop(24, 4, 37, 2, "Capitol Hill"),
+    new Shop(10, 9, 23, 6.33, "South Lake Union"),
+    new Shop(7, 2, 28, 1.25, "Wedgewood"),
+    new Shop(10, 8, 58, 3.75, "Ballard")
 ]
 
-for(var i =0; i < shops.length; i++) {
-	var shop = shops[i];
-	$("#accordion").append(
- 		"<h3>"+shop.name+"</h3>" +
- 		"<div>" +
-	  		"<p>Customer's We Serve Per Hour:  <span>"+average(shop.customersPerHour).toFixed(0)+"</span></p>" +
-	  		"<p>Donut's We Sell Per Hour: <span>"+average(shop.donutsPerHour).toFixed(0)+"</span></p>" +
-	  		"<p>Donut's We Sell Per Day: <span>"+shop.donutsPerDay.toFixed(0)+"</span></p>" +
-  		"</div>"
-	);
+for (var i = 0; i < shops.length; i++) {
+    var shop = shops[i];
+    $("#accordion").append(
+        "<h3>" + shop.name + "</h3>" +
+        "<div>" +
+        "<p>Customer's We Average Per Hour:  <span>" + average(shop.customersPerHour).toFixed(0) + "</span></p>" +
+        "<p>Donut's We Sell Per Hour: <span>" + average(shop.donutsPerHour).toFixed(0) + "</span></p>" +
+        "<p>Donut's We Sell Per Day: <span>" + shop.donutsPerDay.toFixed(0) + "</span></p>" +
+        "</div>"
+    );
 }
 $('#accordion').accordion({
-	active:false,
-	collapsible: true
+    active: false,
+    collapsible: true
 });
 
 $("#donut").draggable({
-  stop: function(event, ui){
-  	console.log (arguments)
-  }
+    stop: function(event, ui) {
+        console.log(arguments)
+    }
 });
 $("#homer").droppable({
-  accept: "#donut",
-  drop: function(){
-  	$('#donut').remove();
-  	alert("Mmm...Donuts!")
-  }});
+    accept: "#donut",
+    drop: function() {
+        $('#donut').remove();
+        alert("Mmm...Donuts!")
+    }
+});
 
-  $("#submit").on("click", function(){
-  	var name = $('#name').val();
-  	var comment = $('#comments').val();
-  	$('#name').val("");
-  	$('#comments').val("");
-    $(".comments").append("<div class='comment'>"+name+" : <span>"+comment+"</span></div>");
-   });
-
+$("form").on("submit",handleSubmitComment);
+$("#comments").on("keydown", function(event){
+    if(event.keyCode === 13) {
+        handleSubmitComment(event)
+    }
+});
+function handleSubmitComment(event) {
+    event.preventDefault();
+    var name = $('#name').val().trim();
+    var comment = $('#comments').val().trim();
+    if (name && comment) {
+        $('#name').val("");
+        $('#comments').val("");
+        $(".comments").prepend("<div class='comment'>" + name + " : <span>" + comment + "</span></div>");
+    }
+}
